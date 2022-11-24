390e7e0dd61bb846aae8a01a54082f50bf2c74cb
Nikhil M <nikhil.m@mca.christuniversity.in>
2022-11-24 20:52:31 +0530

    6 commit 8

--- mcarrAPI/urls.py
+++ mcarrAPI/urls.py
@@ -19,12 +19,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.views.static import serve
-from django.conf.urls import url
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('myapp.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
